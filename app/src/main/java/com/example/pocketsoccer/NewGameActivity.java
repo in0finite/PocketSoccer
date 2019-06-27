8cb862dcfd9c3ba90d92e2a48ccaadc643c40f0d
in0finite <leshow816@gmail.com>
2019-06-28 00:32:31 +0200

    ...

--- app/src/main/java/com/example/pocketsoccer/NewGameActivity.java
+++ app/src/main/java/com/example/pocketsoccer/NewGameActivity.java
@@ -3,6 +3,7 @@ package com.example.pocketsoccer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class NewGameActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class NewGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
+
+        View viewById = this.findViewById(R.id.scrollView1);
+        View viewById2 = this.findViewById(R.id.scrollView2);
+
+        // populate scroll views with flags
+        
     }
 }
