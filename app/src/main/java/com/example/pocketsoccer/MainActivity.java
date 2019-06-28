d2d8f101da5b3013e995788634772990c90b0e06
in0finite <leshow816@gmail.com>
2019-06-28 03:00:23 +0200

    start new game activity

--- app/src/main/java/com/example/pocketsoccer/MainActivity.java
+++ app/src/main/java/com/example/pocketsoccer/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.pocketsoccer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button newGameButton = findViewById(R.id.button2);
+
+        newGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startNewGame();
+            }
+        });
+
+
     }
+
+    void startNewGame() {
+
+        Intent intent = new Intent(this, NewGameActivity.class);
+        startActivity(intent);
+
+    }
+
 }
