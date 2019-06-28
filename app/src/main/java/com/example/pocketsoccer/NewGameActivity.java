30051343363b3abd7ffe7b9036da139f6cc79665
in0finite <leshow816@gmail.com>
2019-06-28 02:20:41 +0200

    populate scroll views with flags

--- app/src/main/java/com/example/pocketsoccer/NewGameActivity.java
+++ app/src/main/java/com/example/pocketsoccer/NewGameActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 
 public class NewGameActivity extends AppCompatActivity {
 
@@ -12,10 +15,26 @@ public class NewGameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
 
-        View viewById = this.findViewById(R.id.scrollView1);
-        View viewById2 = this.findViewById(R.id.scrollView2);
+        ScrollView viewById1 = this.findViewById(R.id.scrollView1);
+        ScrollView viewById2 = this.findViewById(R.id.scrollView2);
 
         // populate scroll views with flags
-        
+
+        int[] imageIds = new int[]{R.drawable.en, R.drawable.fr, R.drawable.arg, R.drawable.br, R.drawable.es, R.drawable.ger, R.drawable.rs};
+
+        for (int imageId : imageIds) {
+            ((ViewGroup) viewById1.getChildAt(0)).addView(createFlag(imageId));
+            ((ViewGroup) viewById2.getChildAt(0)).addView(createFlag(imageId));
+        }
+
     }
+
+    ImageView createFlag(int imageId) {
+        ImageView imageView = new ImageView(getApplicationContext());
+        //imageView.setLayoutParams(new ViewGroup.LayoutParams());
+        imageView.setMaxHeight(150);
+        imageView.setImageResource(imageId);
+        return imageView;
+    }
+
 }
