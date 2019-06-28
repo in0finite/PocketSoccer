bd34681ca41f39dc148f043506a9dfdeb788aef4
in0finite <leshow816@gmail.com>
2019-06-28 20:57:11 +0200

    create custom view

--- app/src/main/java/com/example/pocketsoccer/SoccerActivity.java
+++ app/src/main/java/com/example/pocketsoccer/SoccerActivity.java
@@ -3,6 +3,8 @@ package com.example.pocketsoccer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class SoccerActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SoccerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_soccer);
+
+
+        View view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.sample_soccer_field_view, null);
+
+
+
     }
 }
