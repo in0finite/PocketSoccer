1e75fad136a2f4605050246b12f3f7c5d0db9fc5
in0finite <leshow816@gmail.com>
2019-07-02 00:23:30 +0200

    working on continuing the game

--- app/src/main/java/com/example/pocketsoccer/MainActivity.java
+++ app/src/main/java/com/example/pocketsoccer/MainActivity.java
@@ -7,9 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = "PocketSoccer";
+    public static final String savedGameFileName = "SavedGame.bin";
+
+    Button mContinueGameButton;
 
 
     @Override
@@ -18,8 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Button newGameButton = findViewById(R.id.newGameButton);
-
+        Button newGameButton = this.findViewById(R.id.newGameButton);
         newGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,6 +35,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mContinueGameButton = this.findViewById(R.id.continueGameButton);
+        mContinueGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                continueGame();
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        // disable button if saved game does not exist
+        if (! getSavedGameFile().exists()) {
+            mContinueGameButton.setEnabled(false);
+        }
 
     }
 
@@ -35,6 +63,27 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, NewGameActivity.class);
         startActivity(intent);
 
+    }
+
+    void continueGame() {
+
+        try {
+            File file = getSavedGameFile();
+            if (file.exists()) {
+                byte[] data = Util.readFile(file);
+
+                Intent intent = new Intent(this, NewGameActivity.class);
+                intent.putExtra("continueGameData", data);
+                startActivity(intent);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public File getSavedGameFile() {
+        return new File(getFilesDir().getPath() + "/" + savedGameFileName);
     }
 
 }
--- app/src/main/java/com/example/pocketsoccer/Util.java
+++ app/src/main/java/com/example/pocketsoccer/Util.java
@@ -1,9 +1,22 @@
 package com.example.pocketsoccer;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Util {
 
     public static int randomInt(int min, int max) {
         return min + (int) Math.round( Math.random() * (max - min) );
+    }
+
+    public static byte[] readFile(File file) throws IOException {
+        byte[] data = new byte[(int) file.length()];
+        FileInputStream fis = new FileInputStream(file);
+        fis.read(data);
+        fis.close();
+        return data;
     }
 
 }
