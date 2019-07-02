62d2a92b9e6effb4b3bbae6cb407c83abe0a72a9
in0finite <leshow816@gmail.com>
2019-07-02 20:05:41 +0200

    create entity and dao classes

---
+++ app/src/main/java/com/example/pocketsoccer/db/Game.java
@@ -0,0 +1,33 @@
+package com.example.pocketsoccer.db;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Date;
+
+@Entity
+public class Game {
+
+    @PrimaryKey
+    public int uid;
+
+    @ColumnInfo(name = "player1Name")
+    public String player1Name;
+
+    @ColumnInfo(name = "player2Name")
+    public String player2Name;
+
+    @ColumnInfo(name = "player1Score")
+    public int player1Score;
+
+    @ColumnInfo(name = "player2Score")
+    public int player2Score;
+
+    @ColumnInfo(name = "timeElapsed")
+    public float timeElapsed;
+
+    @ColumnInfo(name = "timeWhenFinished")
+    public Date timeWhenFinished;
+
+}
---
+++ app/src/main/java/com/example/pocketsoccer/db/GameDao.java
@@ -0,0 +1,29 @@
+package com.example.pocketsoccer.db;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface GameDao {
+
+    @Query("SELECT * FROM game")
+    List<Game> getAll();
+
+    @Query("SELECT * FROM game WHERE uid IN (:gameIds)")
+    List<Game> loadAllByIds(int[] gameIds);
+
+    @Query("SELECT * FROM game WHERE player1Name = :player1 AND " +
+            "player2Name = :player2")
+    List<Game> find(String player1, String player2);
+
+    @Insert
+    void insertAll(Game... games);
+
+    @Delete
+    void delete(Game game);
+
+}
