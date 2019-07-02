10257c6d48a63a61d7f1fd575c42f3148b236111
in0finite <leshow816@gmail.com>
2019-07-02 21:51:05 +0200

    working on game stats

--- app/src/main/java/com/example/pocketsoccer/StatsForSingleGameActivity.java
+++ app/src/main/java/com/example/pocketsoccer/StatsForSingleGameActivity.java
@@ -2,7 +2,13 @@ package com.example.pocketsoccer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.pocketsoccer.db.AppDatabase;
+import com.example.pocketsoccer.db.Game;
+
+import java.util.List;
 
 public class StatsForSingleGameActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class StatsForSingleGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats_for_single_game);
+
+        Intent intent = this.getIntent();
+
+        String player1Name = intent.getStringExtra("player1Name");
+        String player2Name = intent.getStringExtra("player2Name");
+
+        int player1Score = intent.getIntExtra("player1Score", 0);
+        int player2Score = intent.getIntExtra("player2Score", 0);
+
+        float timeElapsed = intent.getFloatExtra("timeElapsed", 0f);
+        long timeWhenFinished = intent.getLongExtra("timeWhenFinished", 0);
+
+        List<Game> games = AppDatabase.getInstance(this).gameDao().find(player1Name, player2Name);
+
+        // populate UI
+
+
+
     }
 }
